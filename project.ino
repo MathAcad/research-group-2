862696c73898866d3fe3b0d4e3e7be35d9ba1f22
MathAcad <145073932+MathAcad@users.noreply.github.com>
2023-12-11 15:34:33 +0900

    draft (might not work)

--- project.ino
+++ project.ino
@@ -1,132 +1,174 @@
+// Practical Research 2
+
+// include library
+// servo motor library
 #include <Servo.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#include <LiquidCrystal.h>
-const int rs = 49, en = 45, d4 = 35, d5 = 33, d6 = 31, d7 = 29;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-#define DHTPIN 2     // Digital pin connected to the DHT sensor 
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-DHT_Unified dht(DHTPIN, DHTTYPE);
-uint32_t delayMS;
-Servo servo;
-int angle = 10;
+
+// Plastic bin pin definitions
+#define redpin 5
+#define bluepin 6
+#define greenpin 7
+#define plasticsensor A0
+#define metalsensor A1
+#define IR_plastic A3
+
+// Metal bin pin definitions
+#define redpin_M 8
+#define bluepin_M 9
+#define greenpin_M 10
+#define metalsensor_M A2
+#define IR_metal A3
+
+// Create servo object to control a servo on plastic bin
+Servo myservo;
+// create servo object to control a servo on metal bin
+Servo myservo_M;
+
+
+// deafult servo position of plastic bin
+int pos = ""; // 165
+// deafult servo position of metal bin
+int pos_M = ""; //158
+
 
 void setup() {
-	pinMode(5, INPUT);
-	servo.attach(8);
-	servo.write(angle);
+	// Attaches the plastic bin servo on pin 11 to the servo object
+	myservo.attach(11);
+	
+	// Attaches the metal bin servo on pin 12 to the servo object
+	myservo_M.attach(12);
+	
+	// RGB LED on plastic bin
+	pinMode(redpin,OUTPUT);
+	pinMode(bluepin,OUTPUT);
+	pinMode(greenpin,OUTPUT);
+
+	// RGB LED on metal bin
+	pinMode(redpin_M,OUTPUT);
+	pinMode(bluepin_M,OUTPUT);
+	pinMode(greenpin_M,OUTPUT);
+	
+	// Create sensor values in pull up condition
+	pinMode(plasticsensor,INPUT_PULLUP);
+	pinMode(metalsensor,INPUT_PULLUP);
+	pinMode(metalsensor_M,INPUT_PULLUP);
+	
+	// Activate IR sensor
+	pinMode(IR_plastic,INPUT);
+	pinMode(IR_metal,INPUT);
+	
+	// Start serial monitor
 	Serial.begin(9600);
-	dht.begin();
-	sensor_t sensor;
-	dht.temperature().getSensor(&sensor);
 	
-	int lcdVDD = 53;
-	pinMode(lcdVDD, OUTPUT);
-	digitalWrite(lcdVDD, HIGH);
+	//Set RGB LED on deafult vaues (white)
+	analogWrite(redpin,255);
+	analogWrite(bluepin,255);
+	analogWrite(greenpin,255);
+	analogWrite(redpin_M,255);
+	analogWrite(bluepin_M,255);
+	analogWrite(greenpin_M,255);
+}
+
+
+void loop() {
+	int sensor_read_plastic = digitalRead(plasticsensor);
+	int sensor_read_metal = digitalRead(metalsensor);
+	int sensor_read_metaletal2 = digitalRead(metalsensor_M);
+	int sensor_read_IR_plastic = digitalRead(IR_plastic);
+	int sensor_read_IR_metal = digitalRead(IR_metal);
+	Serial.println("plastic sensor");
+	Serial.println(sensor_read_plastic);
+	Serial.println("metal sensor");
+	Serial.println(sensor_read_metal);
+	Serial.println(sensor_read_metaletal2);
 	
-	int lcdV0 = 51;
-	pinMode(lcdV0, OUTPUT);
-	digitalWrite(lcdV0, LOW);
-
-	int lcdRW = 47;
-	pinMode(lcdRW, OUTPUT);
-	digitalWrite(lcdRW, LOW);
-  
-	lcd.begin(16, 2);
-	// Print a message to the LCD.
-	lcd.print("Hello Welcome");
-	lcd.setCursor(0,1);
-	lcd.print("We are Group 2 Practical Research");
-	/* * LCD RS pin to digital pin 12
-	* LCD Enable pin to digital pin 11
-	* LCD D4 pin to digital pin 6
-	* LCD D5 pin to digital pin 4
-	* LCD D6 pin to digital pin 3
-	* LCD D7 pin to digital pin 1
-	* LCD R/W pin to ground
-	* LCD VSS pin to ground
-	* LCD VCC pin to 5V
-	* 10K resistor:
-	* ends to +5V and ground
-	* wiper to LCD VO pin (pin 3) */
-}
-
-int B=0;
-void loop() 
-{ 
-	// read obstacle status and store it into "detect"
-	int detect = digitalRead(5);
-	if (detect == LOW) {
-		B=1;
-		Serial.println("Obastacle on the way");
+	// Check if the trash bin is full
+	// Plastic bin
+	if(sensor_read_IR_plastic == 1) {
+		analogWrite(redpin,255);
+		analogWrite(bluepin,0);
+		analogWrite(greenpin,0);
 	}
-	if (B==1) {
-		amanMoisture(B);
+	
+	// Metal bin
+	if(sensor_read_IR_metal == 1){
+		analogWrite(redpin_M,255);
+		analogWrite(bluepin_M,0);
+		analogWrite(greenpin_M,0);
 	}
-	B=0;
-	sensors_event_t event;
-	dht.humidity().getEvent(&event);
-	float P = event.relative_humidity;
-	Serial.print(F("Humidity: "));
-	Serial.print(P);
-	Serial.println(F("%"));
-	delay(1000);
-}
-
-void amanMoisture(int B)
-{
-	sensors_event_t eventf;
-    dht.humidity().getEvent(&eventf);   
-	float Hf = eventf.relative_humidity;
-	Serial.print(F("Humidity first: "));
-    Serial.print(Hf);
-    Serial.println(F("%"));
-    delay(8000);
-    sensors_event_t eventfake;
-    dht.humidity().getEvent(&eventfake);
-    float Hfake = eventfake.relative_humidity;
-    Serial.print(F("Humidity Fake: "));
-    Serial.print(Hfake);
-    Serial.println(F("%"));
-    delay(2000);
-    sensors_event_t eventl;
-    dht.humidity().getEvent(&eventl);
-    float Hl = eventl.relative_humidity;
-    Serial.print(F("Humidity last: "));
-    Serial.print(Hl);
-    Serial.println(F("%"));
-    amanMotor(Hf, Hl);
-}
-
-void amanMotor(float Hf, float Hl)
-{
-	if(Hl > Hf + 3) {
-		lcd.clear();
-		lcd.setCursor(0,1);
-		lcd.print("WET");
-		for(angle = 90; angle > 0; angle--) {
-			servo.write(angle);               
-			delay(15); 
-		} 
-		delay(200);
-		for(angle = 0; angle < 90; angle++) {
-			servo.write(angle);
-			delay(15);
+	
+	// plastic bin
+	// might be wrong condition
+	if((sensor_read_plastic == 0) && (sensor_read_metal != 1)){
+		// (160° to 0°)
+		// tweak the ranging pos
+		for (pos = 160; pos >= 90; pos -= 1) {
+			// in steps of 1 degree
+			// tell servo to go to position in variable 'pos'
+			myservo.write(pos);
+			// waits 1ms for the servo to reach the position
+			delay(1);
 		}
-		delay(200);
-	} else {
-		lcd.clear();
-		lcd.setCursor(0,1);
-		lcd.print("WET");
-		for(angle = 90; angle < 180; angle++) {
-			servo.write(angle);
-			delay(15);
+		analogWrite(redpin,255);
+		analogWrite(bluepin,0);
+		analogWrite(greenpin,255);
+		delay(2500);
+		// (90° to 160°) unsure
+		// tweak the ranging pos
+		for (pos = ""; pos <= ""; pos += 1) {
+			// tell servo to go to position in variable 'pos'
+			myservo.write(pos);
+			// waits 1ms for the servo to reach the position
+			delay(1);
 		}
-		delay(200);
-		for(angle = 180; angle > 90; angle--) {
-			servo.write(angle);
-			delay(15);
+		analogWrite(redpin,0);
+		analogWrite(bluepin,0);
+		analogWrite(greenpin,0);
+	}
+	else {
+		// keep the door close
+		myservo.write(pos); 
+		
+		analogWrite(redpin,255);
+		analogWrite(bluepin,255);
+		analogWrite(greenpin,255);
+	
+	}
+	
+	//metal bin
+	if(sensor_read_metaletal2 == 1){
+		// (160° to 90°)
+		// tweak it
+		for (pos_M = ""; pos_M >= ""; pos_M -= 1) {
+			// in steps of 1 degree
+			// tell servo to go to position in variable 'pos'
+			myservo_M.write(pos_M);
+			// waits 1ms for the servo to reach the position
+			delay(1);
 		}
-		delay(200);
-    }
+		analogWrite(redpin_M,255);
+		analogWrite(bluepin_M,0);
+		analogWrite(greenpin_M,255);
+		delay(2500);
+		// 90° to 160°
+		// tweak it
+		for (pos_M = 90; pos_M <= 160; pos_M += 1) {
+			// tell servo to go to position in variable 'pos'
+			myservo_M.write(pos_M);
+			// waits 1ms for the servo to reach the position
+			delay(1);
+	
+		}
+		analogWrite(redpin_M,0);
+		analogWrite(bluepin_M,0);
+		analogWrite(greenpin_M,0);
+	}
+	else {
+		// keep the door close
+		myservo_M.write(pos_M); 
+		
+		analogWrite(redpin_M,255);
+		analogWrite(bluepin_M,255);
+		analogWrite(greenpin_M,255);
+	}
 }
