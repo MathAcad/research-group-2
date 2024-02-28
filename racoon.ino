061dfbac1f1520dadcb7cb82af84f57d0312823d
MathAcad <145073932+MathAcad@users.noreply.github.com>
2024-02-28 17:39:10 +0800

    rqacoon

--- racoon.ino
+++ racoon.ino
@@ -1,27 +1,23 @@
 #include<Stepper.h>
-// #include <Servo.h>
 
 #define pin_prox 2
 #define pin_in1 8
 #define pin_in2 4
 #define pin_in3 7
 #define pin_in4 3
-// #define pin_servo1 3
-// #define pin_servo2 4
 #define pin_trig 5
 #define pin_echo 6
 
 const float stepper_rev = 32;
-const float gear_ratio = 64;
-const float step_rev = stepper_rev * gear_ratio;
+const float gear_ratio = 10;
+const float step_rev = 2048;
+// const float step_rev = stepper_rev * gear_ratio;
 
 Stepper mystepper(stepper_rev, pin_in1, pin_in2, pin_in3, pin_in4);
 long duration;
 int distance;
 int pos = 0;
 int steps_req;
-// Servo myservo1;
-// Servo myservo2;
 
 void setup()
 {
@@ -32,8 +28,6 @@ void setup()
    pinMode(pin_trig,OUTPUT);
    pinMode(pin_echo,INPUT);
    Serial.begin(9600);
-  //  myservo1.attach(pin_servo1);
-  //  myservo2.attach(pin_servo2);
 }
   
 void loop()
@@ -49,46 +43,40 @@ void loop()
 
    // non-metal
    if (distance <= 9 && sensorValue_metal == 0) {
+      steps_req = 0;
       Serial.println("object detected non-metal");
       Serial.println(distance);
       Serial.println("Anti-Clockwise");
       steps_req = step_rev / 2;
-      mystepper.setSpeed(1000);
+      mystepper.setSpeed(600);
       mystepper.step(steps_req);
-      delay(2000);
+      delay(1000);
       Serial.println("Clockwise");
-      steps_req = -step_rev / 2;
-      Serial.println("Val" + steps_req);
-      mystepper.setSpeed(1000);
+      steps_req = -290;
+      // steps_req = -step_rev / 2;
+      // Serial.println("Clockwise" + steps_req);
+      mystepper.setSpeed(600);
       mystepper.step(steps_req);
-      delay(2000);
-
-
-
-
-      // for (pos = 90; pos <= 135; pos += 1) {
-      //   myservo1.write(pos);
-      //   delay(1);
-      // }
-      // delay(3000);
-      // for (pos = 135; pos >= 90; pos -= 1) {
-      //   myservo1.write(pos);
-      //   delay(1);
-      // }
+      // int steps_req = 0;
    // metal
    } else if (sensorValue_metal != 0) {
+      steps_req = 0;
       Serial.println("object detected metal");
-      // for (pos = 90; pos <= 135; pos += 1) {
-      //   myservo2.write(pos);
-      //   delay(1);
-      // }
-      // delay(3000);
-      // for (pos = 135; pos >= 90; pos -= 1) {
-      //   myservo2.write(pos);
-      //   delay(1);
-      // }
+      Serial.println("Clockwise");
+      // steps_req = -step_rev / 2;
+      steps_req = -285;
+      
+      mystepper.setSpeed(600);
+      mystepper.step(steps_req);
+      delay(1000);
+      Serial.println("Anti-Clockwise");
+      steps_req = 310;
+      // steps_req = step_rev / 2;
+      mystepper.setSpeed(600);
+      mystepper.step(steps_req);
+      // int steps_req = 0;
    } else {
       Serial.println("no metal or non-metal object detected");
    }
-   delay(1000);
+   delay(500);
 }
